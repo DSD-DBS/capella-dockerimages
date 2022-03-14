587053632e6ae7622f8ace09296f8f7a777656b2
MoritzWeber0 <kontakt@moritz-weber.net>
2022-03-14 15:44:47 +0100

    Support root as additional startup user

--- remote/startup.sh
+++ remote/startup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
-echo -e "tmp_passwd\n$RMT_PASSWORD\n$RMT_PASSWORD" | passwd;
+if [ "$(whoami)" == "root" ];
+then 
+    echo -e "$RMT_PASSWORD\n$RMT_PASSWORD" | passwd techuser;
+elif [ "$(whoami)" == "techuser" ];
+then
+    echo -e "tmp_passwd\n$RMT_PASSWORD\n$RMT_PASSWORD" | passwd;
+else
+    echo "Only techuser and root are supported as users.";
+    exit 1;
+fi
+
 python3 /opt/setup_workspace.py;
 
 # Replace environment variables in capella.ini, e.g. licences
