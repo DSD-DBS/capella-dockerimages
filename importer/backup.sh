c0aedccfc6329ef2b3d7c27395435f824e7c8922
Arjan Molenaar <gaphor@gmail.com>
2022-03-15 15:52:37 +0100

    Print commands as they are executed in backup script

--- importer/backup.sh
+++ importer/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 mkdir /tmp/model;
 ./importer.sh \
@@ -19,9 +19,6 @@ cd /tmp/git;
 git switch $GIT_REPO_BRANCH || git switch -c $GIT_REPO_BRANCH;
 
 export T4C_PROJECT_NAME_CLEANED=$(echo "$T4C_PROJECT_NAME" | sed -e "s/%20/ /g")
-
-ls -l /tmp/model || true
-ls -l /tmp/model/* || true
 
 /bin/cp -rf "/tmp/model/$T4C_PROJECT_NAME_CLEANED/"* /tmp/git;
 
