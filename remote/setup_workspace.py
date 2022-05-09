5f177d7737990e566633eb5c5ff3d612c66614ef
ewuerger <ernst.wuerger@gmail.com>
2022-05-09 13:20:14 +0200

    Applied black and remove obsolete import

--- remote/setup_workspace.py
+++ remote/setup_workspace.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import glob
 import logging
 import os
 import pathlib
@@ -59,7 +58,7 @@ def setup_repositories() -> None:
             / "fr.obeo.dsl.viewpoint.collab"
             / "repository.properties",
             repo,
-            fr"tcp\://{t4c_host}\:{t4c_port}/{repo}",
+            rf"tcp\://{t4c_host}\:{t4c_port}/{repo}",
         )
 
 
