3aa38a78a11cf2bde249e51207a3a011db0a8420
ewuerger <ernst.wuerger@gmail.com>
2022-05-13 11:47:38 +0200

    Add idletime no display handling

--- remote/metrics.py
+++ remote/metrics.py
@@ -2,6 +2,7 @@
 
 import os
 import subprocess
+from datetime import datetime, timedelta
 
 from prometheus_client import Gauge, start_http_server
 
@@ -12,17 +13,38 @@ METRICS_PORT = int(
 IDLETIME = Gauge("idletime_seconds", "Idletime of X server in seconds from xprintidle")
 
 
-def get_idletime() -> float:
-    """Return output of xprintidle (Idle time of X server in millisecs)"""
-    proc = subprocess.run(
-        ["xprintidle"], check=True, capture_output=True, encoding="utf-8"
-    )
-    return round(int(proc.stdout) / 1000, 2)
+class IdleTime:
+    """Measure the idle time of the X server even if there is no X server."""
+
+    def __init__(self) -> None:
+        self.last_checkpoint = (datetime.now(), 0.0)
+
+    def get_idletime(self) -> float:
+        """
+        Return idle time in seconds.
+
+        If there us an X server return output of xprintidle
+        (Idle time of X server in millisecs) in seconds. If there is no
+        display the idle time increases. Then the timedelta from now and
+        the last successfull xprintidle exec is returned.
+        """
+        proc = subprocess.run(
+            ["xprintidle"], check=True, capture_output=True, encoding="utf-8"
+        )
+        if proc.stdout == "couldn't open display":
+            checkpoint, idle_time = self.last_checkpoint
+            delta = datetime.now() - checkpoint
+            return (delta + timedelta(seconds=idle_time)).seconds
+
+        current_idle_time = float(proc.stdout)
+        self.last_checkpoint = (datetime.now(), current_idle_time)
+        return round(current_idle_time / 1000, 2)
 
 
-IDLETIME.set_function(get_idletime)
+idler = IdleTime()
 
 if __name__ == "__main__":
+    IDLETIME.set_function(idler.get_idletime)
     start_http_server(METRICS_PORT)
     while True:
         ...  # Prometheus metric server dies if there is no run_forever mechanism
--- remote/tests/test_metrics.py
+++ remote/tests/test_metrics.py
@@ -1,16 +1,41 @@
+import time
 from unittest import mock
 
 import pytest
-from metrics import get_idletime
+from metrics import IdleTime
+
+# pylint: disable=redefined-outer-name
 
 
-@mock.patch("subprocess.run", lambda _: "Could not open display")
-def test_get_idletime_fails_when_no_display_is_found() -> None:
+@pytest.fixture
+def idler() -> IdleTime:
+    """Return an instance of `metrics.IdleTime`"""
+    return IdleTime()
+
+
+no_display_stdouter = lambda _: "could not open display"
+
+
+@mock.patch("subprocess.run", no_display_stdouter)
+def test_get_idletime_fails_when_no_display_is_found(idler: IdleTime) -> None:
     with pytest.raises(TypeError):
-        get_idletime()
+        idler.get_idletime()
 
 
-def test_get_idletime_works() -> None:
-    idletime = get_idletime()
+def test_get_idletime_works(idler: IdleTime) -> None:
+    idletime = idler.get_idletime()
 
     assert isinstance(idletime, float)
+
+
+def test_get_idletime_increases_after_display_is_closed(idler: IdleTime) -> None:
+    idletime = idler.get_idletime()
+
+    time.sleep(10)
+
+    mock.patch("subprocess.run", no_display_stdouter)
+
+    second_idletime = idler.get_idletime()
+
+    assert isinstance(idletime, float) and isinstance(second_idletime, float)
+    assert idletime < second_idletime
