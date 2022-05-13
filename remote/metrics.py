6afacb7e34c1936302def40b6ab342876d6d3973
ewuerger <ernst.wuerger@gmail.com>
2022-05-13 13:16:25 +0200

    Add never had a display use case handling
    - When there was no display on init of IdleTimer the idletime is set to
    -1.0 and stays there if no X server is spawned
    - When X server is spawned xprintidle is used.
    - When X server is killed the idletime is increased via timedeltas of
    checkpoints

--- remote/metrics.py
+++ remote/metrics.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import logging
 import os
 import subprocess
 from datetime import datetime, timedelta
@@ -9,15 +10,34 @@ from prometheus_client import Gauge, start_http_server
 METRICS_PORT = int(
     os.getenv("METRICS_PORT", 9118)  # pylint: disable=invalid-envvar-default
 )
-
+LOGGER = logging.getLogger(__file__)
 IDLETIME = Gauge("idletime_seconds", "Idletime of X server in seconds from xprintidle")
 
 
-class IdleTime:
+class IdleTimer:
     """Measure the idle time of the X server even if there is no X server."""
 
     def __init__(self) -> None:
-        self.last_checkpoint = (datetime.now(), 0.0)
+        now = datetime.now()
+        self.first_checkpoint = (now, self._xprintidle(init=True) or -1.0)
+        self.last_checkpoint = self.first_checkpoint
+
+    def _xprintidle(self, *, init: bool = False) -> float:
+        proc = subprocess.run(
+            ["xprintidle"], check=False, capture_output=True, encoding="utf-8"
+        )
+        LOGGER.debug("xprintidle's stdout: '%s'", proc.stdout)
+        LOGGER.debug("xprintidle's stderr: '%r'", proc.stderr)
+        if proc.stdout in {"couldn't open display", ""}:
+            if init or self.last_checkpoint[1] == -1.0:
+                LOGGER.debug("no display on init")
+                return -1.0
+
+            checkpoint, idle_time = self.last_checkpoint
+            delta = datetime.now() - checkpoint
+            return (delta + timedelta(seconds=idle_time)).seconds
+
+        return float(proc.stdout) / 1000
 
     def get_idletime(self) -> float:
         """
@@ -28,20 +48,12 @@ class IdleTime:
         display the idle time increases. Then the timedelta from now and
         the last successfull xprintidle exec is returned.
         """
-        proc = subprocess.run(
-            ["xprintidle"], check=True, capture_output=True, encoding="utf-8"
-        )
-        if proc.stdout == "couldn't open display":
-            checkpoint, idle_time = self.last_checkpoint
-            delta = datetime.now() - checkpoint
-            return (delta + timedelta(seconds=idle_time)).seconds
-
-        current_idle_time = float(proc.stdout)
+        current_idle_time = self._xprintidle()
         self.last_checkpoint = (datetime.now(), current_idle_time)
-        return round(current_idle_time / 1000, 2)
+        return round(current_idle_time, 2)
 
 
-idler = IdleTime()
+idler = IdleTimer()
 
 if __name__ == "__main__":
     IDLETIME.set_function(idler.get_idletime)
--- remote/tests/test_metrics.py
+++ remote/tests/test_metrics.py
@@ -1,39 +1,63 @@
+"""Unit tests for metrics"""
 import time
 from unittest import mock
 
 import pytest
-from metrics import IdleTime
+from metrics import IdleTimer
 
 # pylint: disable=redefined-outer-name
 
 
 @pytest.fixture
-def idler() -> IdleTime:
+def idler() -> IdleTimer:
     """Return an instance of `metrics.IdleTime`"""
-    return IdleTime()
+    return IdleTimer()
 
 
-no_display_stdouter = lambda _: "could not open display"
+def print_no_display(*__, **_) -> str:
+    """
+    Just print: 'could not open display', the response of xprintidle
+    when there is no X server.
+    """
+    return mock.MagicMock(stdout="couldn't open display")
 
 
-@mock.patch("subprocess.run", no_display_stdouter)
-def test_get_idletime_fails_when_no_display_is_found(idler: IdleTime) -> None:
-    with pytest.raises(TypeError):
-        idler.get_idletime()
+def test_get_idletime_had_no_display_on_init_but_later() -> None:
+    with mock.patch("subprocess.run", print_no_display):
+        idler = IdleTimer()
+        _, init_idletime = idler.first_checkpoint
+
+    idletime = idler.get_idletime()
+
+    assert init_idletime == -1
+    assert idletime > -1
 
 
-def test_get_idletime_works(idler: IdleTime) -> None:
+def test_get_idletime_never_had_a_display_and_returns_minus_one() -> None:
+    with mock.patch("subprocess.run", print_no_display):
+        idler = IdleTimer()
+        _, init_idletime = idler.first_checkpoint
+
+        time.sleep(10)
+
+        idletime = idler.get_idletime()
+
+    assert init_idletime == -1
+    assert idletime == -1
+
+
+def test_get_idletime_works(idler: IdleTimer) -> None:
     idletime = idler.get_idletime()
 
     assert isinstance(idletime, float)
 
 
-def test_get_idletime_increases_after_display_is_closed(idler: IdleTime) -> None:
+def test_get_idletime_increases_after_display_is_closed(idler: IdleTimer) -> None:
     idletime = idler.get_idletime()
 
     time.sleep(10)
 
-    mock.patch("subprocess.run", no_display_stdouter)
+    mock.patch("subprocess.run", print_no_display)
 
     second_idletime = idler.get_idletime()
 
