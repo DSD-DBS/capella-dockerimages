c0c78fbbe6b25f04ca898349b0c821a9011f038c
ewuerger <ernst.wuerger@gmail.com>
2022-05-20 11:02:29 +0200

    Change idletimes from seconds to minutes

--- remote/metrics.py
+++ remote/metrics.py
@@ -11,7 +11,7 @@ METRICS_PORT = int(
     os.getenv("METRICS_PORT", 9118)  # pylint: disable=invalid-envvar-default
 )
 LOGGER = logging.getLogger(__file__)
-IDLETIME = Gauge("idletime_seconds", "Idletime of X server in seconds from xprintidle")
+IDLETIME = Gauge("idletime_minutes", "Idletime of X server in minutes from xprintidle")
 
 
 class IdleTimer:
@@ -37,14 +37,14 @@ class IdleTimer:
             delta = datetime.now() - checkpoint
             return (delta + timedelta(seconds=idle_time)).seconds
 
-        return float(proc.stdout) / 1000
+        return float(proc.stdout) / 60000
 
     def get_idletime(self) -> float:
         """
-        Return idle time in seconds.
+        Return idle time in minutes.
 
-        If there us an X server return output of xprintidle
-        (Idle time of X server in millisecs) in seconds. If there is no
+        If there is an X server return output of xprintidle
+        (Idle time of X server in millisecs) in minutes. If there is no
         display the idle time increases. Then the timedelta from now and
         the last successfull xprintidle exec is returned.
         """
