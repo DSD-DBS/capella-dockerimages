3aaddaee33b992ae00e8372a083693709f787f5b
Timothé Bailly-Barthez <timothe.bailly-barthez@deutschebahn.com>
2022-10-28 14:30:56 +0200

    feat: T4C_JSON environment variable handling

--- remote/setup_workspace.py
+++ remote/setup_workspace.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
 # SPDX-License-Identifier: Apache-2.0
 
+from collections import Counter
+import json
 import logging
 import os
 import pathlib
@@ -41,15 +43,29 @@ def setup_repositories() -> None:
     t4c_repositories = os.getenv("T4C_REPOSITORIES", "").split(",")
     t4c_host = os.getenv("T4C_SERVER_HOST", "localhost")
     t4c_port = os.getenv("T4C_SERVER_PORT", 2036)
+    t4c_json = os.getenv("T4C_JSON", None)
 
-    for repo in t4c_repositories:
-        replace_config(
-            REPOSITORIES_BASE_PATH
-            / "fr.obeo.dsl.viewpoint.collab"
-            / "repository.properties",
-            repo,
-            rf"tcp\://{t4c_host}\:{t4c_port}/{repo}",
-        )
+    if t4c_json:
+        t4c_repos: list[dict[str, str]] = json.loads(t4c_json)
+        duplicate_names = [name for name, count in Counter([repo['repository'] for repo in t4c_repos]).items() if count > 1]
+        for repo in t4c_repos:
+            replace_config(
+                REPOSITORIES_BASE_PATH
+                / "fr.obeo.dsl.viewpoint.collab"
+                / "repository.properties",
+                f"{repo['repository']}\\ ({repo['instance']})" if repo['repository'] in duplicate_names else repo['repository'],
+                rf"tcp\://{repo['host']}\:{repo['port']}/{repo['repository']}"
+            )
+
+    else:
+        for repo in t4c_repositories:
+            replace_config(
+                REPOSITORIES_BASE_PATH
+                / "fr.obeo.dsl.viewpoint.collab"
+                / "repository.properties",
+                repo,
+                rf"tcp\://{t4c_host}\:{t4c_port}/{repo}",
+            )
 
 
 if __name__ == "__main__":
