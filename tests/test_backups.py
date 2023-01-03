7ac4d403bdcf53d1a4256dde126c8bf171e116bd
dominik003 <info@dominik-lammers.de>
2023-01-03 13:37:50 +0100

    feat: Add random port assignment

--- tests/test_backups.py
+++ tests/test_backups.py
@@ -1,9 +1,12 @@
 # SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
 # SPDX-License-Identifier: Apache-2.0
 
+from __future__ import annotations
+
 import logging
 import os
 import pathlib
+import re
 import subprocess
 
 import conftest
@@ -17,34 +20,51 @@ CAPELLA_VERSION = os.getenv("CAPELLA_VERSION", "")
 
 
 def is_capella_5_x_x() -> bool:
-    return CAPELLA_VERSION in ("5.0.0", "5.2.0")
+    return bool(re.match(r"5.[0-9]+.[0-9]+", CAPELLA_VERSION))
 
 
 def is_capella_6_x_x() -> bool:
-    return CAPELLA_VERSION in ("6.0.0")
+    return bool(re.match(r"6.[0-9]+.[0-9]+", CAPELLA_VERSION))
 
 
 @pytest.fixture(name="local_git_container")
 def fixture_local_git_container() -> containers.Container:
-    ports: dict[str, int] = {"80/tcp": 9010}
+    ports: dict[str, int | None] = {"80/tcp": None}
 
     with conftest.get_container(
-        image="tests/local-git-server", ports=ports
+        image="local-git-server", ports=ports
     ) as container:
         conftest.wait_for_container(container, "server started")
         yield container
 
 
 @pytest.fixture(name="t4c_backup_container")
-def fixture_t4c_backup_container(request) -> containers.Container:
+def fixture_t4c_backup_container(
+    local_git_container: containers.Container,
+    t4c_server_container: containers.Container,
+    request,
+) -> containers.Container:
+    t4c_server_container.reload()
+
+    t4c_repo_port = t4c_server_container.ports["2036/tcp"][0]["HostPort"]
+    t4c_cdo_port = t4c_server_container.ports["12036/tcp"][0]["HostPort"]
+
+    if "HTTP_PORT" in request.param:
+        request.param["HTTP_PORT"] = t4c_server_container.ports["8080/tcp"][0][
+            "HostPort"
+        ]
+
+    local_git_container.reload()
+    git_server_port = local_git_container.ports["80/tcp"][0]["HostPort"]
+
     env: dict[str, str] = {
-        "GIT_REPO_URL": "http://localhost:9010/git/git-test-repo.git",
+        "GIT_REPO_URL": f"http://localhost:{git_server_port}/git/git-test-repo.git",
         "GIT_REPO_BRANCH": "backup-test",
         "GIT_USERNAME": "any",
         "GIT_PASSWORD": "any",
         "T4C_REPO_HOST": "127.0.0.1",
-        "T4C_REPO_PORT": "2036",
-        "T4C_CDO_PORT": "12036",
+        "T4C_REPO_PORT": t4c_repo_port,
+        "T4C_CDO_PORT": t4c_cdo_port,
         "T4C_REPO_NAME": "test-repo",
         "T4C_PROJECT_NAME": "test-project",
         "T4C_USERNAME": "admin",
@@ -63,17 +83,17 @@ def fixture_t4c_backup_container(request) -> containers.Container:
 def fixture_t4c_server_container(request) -> containers.Container:
     env: dict[str, str] = {"REST_API_PASSWORD": "password"} | request.param
 
-    ports: dict[str, int] = {
-        "2036/tcp": 2036,
-        "8080/tcp": 8081,
-        "12036/tcp": 12036,
+    ports: dict[str, int | None] = {
+        "2036/tcp": None,
+        "8080/tcp": None,
+        "12036/tcp": None,
     }
 
     with conftest.get_container(
         image="t4c/server/server",
         ports=ports,
         environment=env,
-        path=(pathlib.Path(__file__).parents[0])
+        path=pathlib.Path(__file__).parents[0]
         / "t4c-server-test-data"
         / "data"
         / CAPELLA_VERSION,
@@ -104,7 +124,7 @@ def fixture_t4c_server_container(request) -> containers.Container:
                 "CONNECTION_TYPE": "http",
                 "HTTP_LOGIN": "admin",
                 "HTTP_PASSWORD": "password",
-                "HTTP_PORT": "8081",
+                "HTTP_PORT": None,
             },
             marks=pytest.mark.skipif(
                 condition=is_capella_5_x_x(),
@@ -115,18 +135,18 @@ def fixture_t4c_server_container(request) -> containers.Container:
     indirect=True,
 )
 def test_model_backup_happy(
-    local_git_container,  # pylint: disable=unused-argument
-    t4c_server_container,  # pylint: disable=unused-argument
-    t4c_backup_container,
+    t4c_backup_container: containers.Container,
+    local_git_container: containers.Container,
     tmp_path: pathlib.Path,
 ):
     conftest.wait_for_container(t4c_backup_container, "Backup finished")
 
+    git_server_port = local_git_container.ports["80/tcp"][0]["HostPort"]
     subprocess.run(
         [
             "git",
             "clone",
-            "http://localhost:9010/git/git-test-repo.git",
+            f"http://localhost:{git_server_port}/git/git-test-repo.git",
             tmp_path,
         ],
         check=True,
@@ -149,6 +169,7 @@ def test_model_backup_happy(
 @pytest.mark.parametrize(
     "t4c_server_container,t4c_backup_container",
     [
+        # CONNECTION_TYPE "unknown" is not allowed
         ({"ENABLE_CDO": "true"}, {"CONNECTION_TYPE": "unknown"}),
         pytest.param(
             {"ENABLE_CDO": "false"},
@@ -162,10 +183,6 @@ def test_model_backup_happy(
     ],
     indirect=True,
 )
-def test_model_backup_unhappy(
-    local_git_container,  # pylint: disable=unused-argument
-    t4c_server_container,  # pylint: disable=unused-argument
-    t4c_backup_container,
-):
+def test_model_backup_unhappy(t4c_backup_container):
     with pytest.raises(RuntimeError):
         conftest.wait_for_container(t4c_backup_container, "Backup finished")
