714ad45accd2a51ea41b3b72e8e6d49ce9a1ae64
dominik003 <info@dominik-lammers.de>
2023-01-18 12:06:43 +0100

    refactor: Restructuring of test setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,20 @@
 
 from __future__ import annotations
 
+import collections.abc as cabc
+import contextlib
 import logging
 import os
 import pathlib
+import re
+import shutil
 import time
-from collections.abc import Iterator
-from contextlib import contextmanager
 
 import docker
+import pytest
+import requests
 from docker.models import containers
+from requests import auth
 
 log = logging.getLogger(__file__)
 log.setLevel("DEBUG")
@@ -20,7 +25,23 @@ client = docker.from_env()
 timeout = 120  # Timeout in seconds
 
 
-@contextmanager
+HTTP_LOGIN: str = "admin"
+HTTP_PASSWORD: str = "password"
+
+
+T4C_REPO_HOST: str = "127.0.0.1"
+T4C_REPO_NAME: str = "test-repo"
+T4C_PROJECT_NAME: str = "test-project"
+T4C_USERNAME: str = "admin"
+T4C_PASSWORD: str = "password"
+
+GIT_REPO_BRANCH: str = "backup-test"
+GIT_EMAIL: str = "exporter@example.com"
+GIT_USERNAME: str = "any"
+GIT_PASSWORD: str = "any"
+
+
+@contextlib.contextmanager
 def get_container(
     image: str,
     ports: dict[str, int | None] | None = None,
@@ -28,7 +49,9 @@ def get_container(
     path: pathlib.Path | None = None,
     mount_path: str | None = None,
     network: str | None = None,
-) -> Iterator[containers.Container]:
+    entrypoint: list[str] | None = None,
+    use_docker_prefix: bool = True,
+) -> cabc.Iterator[containers.Container]:
     volumes = (
         {
             str(path): {
@@ -39,16 +62,20 @@ def get_container(
         if path
         else {}
     )
-    docker_prefix = os.getenv("DOCKER_PREFIX", "")
-    docker_tag = os.getenv("DOCKER_TAG", "latest")
+
+    if use_docker_prefix:
+        docker_prefix = os.getenv("DOCKER_PREFIX", "")
+        docker_tag = os.getenv("DOCKER_TAG", "latest")
+        image = f"{docker_prefix}{image}:{docker_tag}"
 
     container = client.containers.run(
-        image=f"{docker_prefix}{image}:{docker_tag}",
+        image=image,
         detach=True,
         ports=ports,
         environment=environment,
         volumes=volumes,
         network=network,
+        entrypoint=entrypoint,
     )
 
     try:
@@ -83,3 +110,131 @@ def wait_for_container(
     else:
         log.error("Log from container: %s", container.logs().decode())
         raise TimeoutError("Timeout while waiting for model loading")
+
+
+def is_capella_5_x_x() -> bool:
+    return bool(re.match(r"5.[0-9]+.[0-9]+", os.getenv("CAPELLA_VERSION", "")))
+
+
+def is_capella_6_x_x() -> bool:
+    return bool(re.match(r"6.[0-9]+.[0-9]+", os.getenv("CAPELLA_VERSION", "")))
+
+
+def get_projects_of_t4c_repository(t4c_http_port: str) -> list[dict[str, str]]:
+    res = requests.get(
+        f"http://localhost:{int(t4c_http_port)}/api/v1.0/projects/{T4C_REPO_NAME}",
+        auth=_get_basic_auth(),
+        timeout=60,
+    )
+    res.raise_for_status()
+    return res.json()["projects"]
+
+
+# In case you encounter a connection error here for the tests, first try:
+# 1. deamon-reload
+# 2. docker restart
+# Related to: https://github.com/testcontainers/testcontainers-go/issues/551
+def create_t4c_repository(t4c_http_port: str):
+    res = requests.post(
+        f"http://localhost:{t4c_http_port}/api/v1.0/repositories",
+        auth=_get_basic_auth(),
+        timeout=60,
+        json={
+            "repositoryName": T4C_REPO_NAME,
+            "authenticationType": "",
+            "datasourceType": "H2_EMBEDDED",
+        },
+    )
+    res.raise_for_status()
+
+    assert res.status_code == 201
+
+
+def _get_basic_auth() -> auth.HTTPBasicAuth:
+    return auth.HTTPBasicAuth(HTTP_LOGIN, HTTP_PASSWORD)
+
+
+@pytest.fixture(name="local_git_container")
+def fixture_local_git_container() -> containers.Container:
+    ports: dict[str, int | None] = {"80/tcp": None}
+
+    with get_container(image="local-git-server", ports=ports) as container:
+        wait_for_container(container, "server started")
+        yield container
+
+
+@pytest.fixture(name="git_port")
+def fixture_git_port(local_git_container: containers.Containe) -> str:
+    local_git_container.reload()
+    return local_git_container.ports["80/tcp"][0]["HostPort"]
+
+
+@pytest.fixture(name="git_general_environment")
+def fixture_git_general_environment(git_port: str) -> dict[str, str]:
+    return {
+        "FILE_HANDLER": "GIT",
+        "GIT_REPO_URL": f"http://localhost:{git_port}/git/git-test-repo.git",
+        "GIT_REPO_BRANCH": GIT_REPO_BRANCH,
+        "GIT_USERNAME": GIT_USERNAME,
+        "GIT_PASSWORD": GIT_PASSWORD,
+    }
+
+
+@pytest.fixture(name="t4c_server_container")
+def fixture_t4c_server_container() -> containers.Container:
+    env: dict[str, str] = {"REST_API_PASSWORD": "password"}
+
+    ports: dict[str, int | None] = {
+        "2036/tcp": None,
+        "8080/tcp": None,
+        "12036/tcp": None,
+    }
+
+    with get_container(
+        image="t4c/server/server", ports=ports, environment=env
+    ) as container:
+        wait_for_container(container, "!MESSAGE CDO server started")
+        container.reload()
+        create_t4c_repository(container.ports["8080/tcp"][0]["HostPort"])
+        yield container
+
+
+@pytest.fixture(name="t4c_server_ports")
+def fixture_t4c_server_ports(
+    t4c_server_container: containers.Container,
+) -> tuple[str, str, str]:
+    t4c_server_container.reload()
+    return (
+        t4c_server_container.ports["2036/tcp"][0]["HostPort"],
+        t4c_server_container.ports["8080/tcp"][0]["HostPort"],
+        t4c_server_container.ports["12036/tcp"][0]["HostPort"],
+    )
+
+
+@pytest.fixture(name="t4c_general_environment")
+def fixture_t4c_general_environment(
+    t4c_server_ports: tuple[str, str, str]
+) -> dict[str, str]:
+    return {
+        "T4C_REPO_HOST": T4C_REPO_HOST,
+        "T4C_REPO_PORT": t4c_server_ports[0],
+        "T4C_CDO_PORT": t4c_server_ports[2],
+        "T4C_REPO_NAME": T4C_REPO_NAME,
+        "T4C_PROJECT_NAME": T4C_PROJECT_NAME,
+        "T4C_USERNAME": T4C_USERNAME,
+        "T4C_PASSWORD": T4C_PASSWORD,
+        "INCLUDE_COMMIT_HISTORY": "false",
+        "LOG_LEVEL": "INFO",
+    }
+
+
+@pytest.fixture(name="t4c_exporter_general_environment")
+def fixture_t4c_exporter_general_environment(
+    t4c_server_ports: tuple[str, str, str],
+    t4c_general_environment: dict[str, str],
+) -> dict[str, str]:
+    return t4c_general_environment | {
+        "HTTP_LOGIN": HTTP_LOGIN,
+        "HTTP_PASSWORD": HTTP_PASSWORD,
+        "HTTP_PORT": t4c_server_ports[1],
+    }
--- tests/test_backups.py
+++ tests/test_backups.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 import logging
 import os
 import pathlib
-import re
+import shutil
 import subprocess
 
 import conftest
@@ -16,71 +16,13 @@ from docker.models import containers
 log = logging.getLogger(__file__)
 log.setLevel("DEBUG")
 
-CAPELLA_VERSION = os.getenv("CAPELLA_VERSION", "")
+pytestmark = pytest.mark.t4c_server
 
 
-def is_capella_5_x_x() -> bool:
-    return bool(re.match(r"5.[0-9]+.[0-9]+", CAPELLA_VERSION))
-
-
-def is_capella_6_x_x() -> bool:
-    return bool(re.match(r"6.[0-9]+.[0-9]+", CAPELLA_VERSION))
-
-
-@pytest.fixture(name="local_git_container")
-def fixture_local_git_container() -> containers.Container:
-    ports: dict[str, int | None] = {"80/tcp": None}
-
-    with conftest.get_container(
-        image="local-git-server", ports=ports
-    ) as container:
-        conftest.wait_for_container(container, "server started")
-        yield container
-
-
-@pytest.fixture(name="t4c_backup_container")
-def fixture_t4c_backup_container(
-    local_git_container: containers.Container,
-    t4c_server_container: containers.Container,
-    request,
+@pytest.fixture(name="t4c_server_container_parametrized")
+def fixture_t4c_server_container_parametrized(
+    tmp_path: pathlib.Path, request
 ) -> containers.Container:
-    t4c_server_container.reload()
-
-    t4c_repo_port = t4c_server_container.ports["2036/tcp"][0]["HostPort"]
-    t4c_cdo_port = t4c_server_container.ports["12036/tcp"][0]["HostPort"]
-
-    if "HTTP_PORT" in request.param:
-        request.param["HTTP_PORT"] = t4c_server_container.ports["8080/tcp"][0][
-            "HostPort"
-        ]
-
-    local_git_container.reload()
-    git_server_port = local_git_container.ports["80/tcp"][0]["HostPort"]
-
-    env: dict[str, str] = {
-        "GIT_REPO_URL": f"http://localhost:{git_server_port}/git/git-test-repo.git",
-        "GIT_REPO_BRANCH": "backup-test",
-        "GIT_USERNAME": "any",
-        "GIT_PASSWORD": "any",
-        "T4C_REPO_HOST": "127.0.0.1",
-        "T4C_REPO_PORT": t4c_repo_port,
-        "T4C_CDO_PORT": t4c_cdo_port,
-        "T4C_REPO_NAME": "test-repo",
-        "T4C_PROJECT_NAME": "test-project",
-        "T4C_USERNAME": "admin",
-        "T4C_PASSWORD": "password",
-        "LOG_LEVEL": "INFO",
-        "INCLUDE_COMMIT_HISTORY": "false",
-    } | request.param
-
-    with conftest.get_container(
-        image="t4c/client/backup", environment=env, network="host"
-    ) as container:
-        yield container
-
-
-@pytest.fixture(name="t4c_server_container")
-def fixture_t4c_server_container(request) -> containers.Container:
     env: dict[str, str] = {"REST_API_PASSWORD": "password"} | request.param
 
     ports: dict[str, int | None] = {
@@ -89,14 +31,21 @@ def fixture_t4c_server_container(request) -> containers.Container:
         "12036/tcp": None,
     }
 
+    server_test_data_path = tmp_path / "test-server-data"
+
+    shutil.copytree(
+        src=pathlib.Path(__file__).parents[0]
+        / "t4c-server-test-data"
+        / "data"
+        / os.getenv("CAPELLA_VERSION", ""),
+        dst=server_test_data_path,
+    )
+
     with conftest.get_container(
         image="t4c/server/server",
         ports=ports,
         environment=env,
-        path=pathlib.Path(__file__).parents[0]
-        / "t4c-server-test-data"
-        / "data"
-        / CAPELLA_VERSION,
+        path=server_test_data_path,
         mount_path="/data",
     ) as container:
         conftest.wait_for_container(
@@ -105,16 +54,67 @@ def fixture_t4c_server_container(request) -> containers.Container:
         yield container
 
 
-@pytest.mark.t4c_server
+@pytest.fixture(name="t4c_server_ports_parametrized")
+def fixture_t4c_server_ports_parametrized(
+    t4c_server_container_parametrized: containers.Container,
+) -> tuple[str, str, str]:
+    t4c_server_container_parametrized.reload()
+    return (
+        t4c_server_container_parametrized.ports["2036/tcp"][0]["HostPort"],
+        t4c_server_container_parametrized.ports["8080/tcp"][0]["HostPort"],
+        t4c_server_container_parametrized.ports["12036/tcp"][0]["HostPort"],
+    )
+
+
+@pytest.fixture(name="t4c_backups_prametrized_environment")
+def fixture_t4c_backups_prametrized_environment(
+    t4c_server_ports_parametrized: tuple[str, str, str],
+) -> dict[str, str]:
+    t4c_repo_port, _, t4c_cdo_port = t4c_server_ports_parametrized
+    return {
+        "T4C_REPO_HOST": conftest.T4C_REPO_HOST,
+        "T4C_REPO_PORT": t4c_repo_port,
+        "T4C_CDO_PORT": t4c_cdo_port,
+        "T4C_REPO_NAME": conftest.T4C_REPO_NAME,
+        "T4C_PROJECT_NAME": conftest.T4C_PROJECT_NAME,
+        "T4C_USERNAME": conftest.T4C_USERNAME,
+        "T4C_PASSWORD": conftest.T4C_PASSWORD,
+        "INCLUDE_COMMIT_HISTORY": "false",
+        "LOG_LEVEL": "INFO",
+    }
+
+
+@pytest.fixture(name="t4c_backup_container_parametrized")
+def fixture_t4c_backup_container(
+    git_general_environment: dict[str, str],
+    t4c_backups_prametrized_environment: dict[str, str],
+    t4c_server_ports_parametrized: tuple[str, str, str],
+    request,
+) -> containers.Container:
+    if "HTTP_PORT" in request.param:
+        request.param["HTTP_PORT"] = t4c_server_ports_parametrized[1]
+
+    env: dict[str, str] = (
+        git_general_environment
+        | t4c_backups_prametrized_environment
+        | request.param
+    )
+
+    with conftest.get_container(
+        image="t4c/client/backup", environment=env, network="host"
+    ) as container:
+        yield container
+
+
 @pytest.mark.parametrize(
-    "t4c_server_container,t4c_backup_container",
+    "t4c_server_container_parametrized,t4c_backup_container_parametrized",
     [
         pytest.param({"ENABLE_CDO": "true"}, {"CONNECTION_TYPE": "telnet"}),
         pytest.param(
             {"ENABLE_CDO": "false"},
             {"CONNECTION_TYPE": "telnet"},
             marks=pytest.mark.skipif(
-                condition=is_capella_6_x_x(),
+                condition=conftest.is_capella_6_x_x(),
                 reason="CDO disabled by default for capella >= 6.0.0",
             ),
         ),
@@ -127,7 +127,7 @@ def fixture_t4c_server_container(request) -> containers.Container:
                 "HTTP_PORT": None,
             },
             marks=pytest.mark.skipif(
-                condition=is_capella_5_x_x(),
+                condition=conftest.is_capella_5_x_x(),
                 reason="conncetion type http is not supported for capella < 6.0.0",
             ),
         ),
@@ -135,11 +135,16 @@ def fixture_t4c_server_container(request) -> containers.Container:
     indirect=True,
 )
 def test_model_backup_happy(
-    t4c_backup_container: containers.Container,
+    t4c_backup_container_parametrized: containers.Container,
     local_git_container: containers.Container,
     tmp_path: pathlib.Path,
 ):
-    conftest.wait_for_container(t4c_backup_container, "Backup finished")
+    conftest.wait_for_container(
+        t4c_backup_container_parametrized, "Backup finished"
+    )
+
+    git_path = tmp_path / "test-git-data"
+    git_path.mkdir()
 
     git_server_port = local_git_container.ports["80/tcp"][0]["HostPort"]
     subprocess.run(
@@ -147,27 +152,26 @@ def test_model_backup_happy(
             "git",
             "clone",
             f"http://localhost:{git_server_port}/git/git-test-repo.git",
-            tmp_path,
+            git_path,
         ],
         check=True,
     )
     try:
         subprocess.run(
-            ["git", "switch", "backup-test"], check=True, cwd=tmp_path
+            ["git", "switch", "backup-test"], check=True, cwd=git_path
         )
     except subprocess.CalledProcessError:
         log.debug("backup failed - backup-test branch does not exists")
         raise
 
-    assert os.path.exists(tmp_path / ".project")
-    assert os.path.exists(tmp_path / "test-project.afm")
-    assert os.path.exists(tmp_path / "test-project.aird")
-    assert os.path.exists(tmp_path / "test-project.capella")
+    assert os.path.exists(git_path / ".project")
+    assert os.path.exists(git_path / "test-project.afm")
+    assert os.path.exists(git_path / "test-project.aird")
+    assert os.path.exists(git_path / "test-project.capella")
 
 
-@pytest.mark.t4c_server
 @pytest.mark.parametrize(
-    "t4c_server_container,t4c_backup_container",
+    "t4c_server_container_parametrized,t4c_backup_container_parametrized",
     [
         # CONNECTION_TYPE "unknown" is not allowed
         ({"ENABLE_CDO": "true"}, {"CONNECTION_TYPE": "unknown"}),
@@ -175,7 +179,7 @@ def test_model_backup_happy(
             {"ENABLE_CDO": "false"},
             {"CONNECTION_TYPE": "telnet"},
             marks=pytest.mark.skipif(
-                condition=is_capella_5_x_x(),
+                condition=conftest.is_capella_5_x_x(),
                 reason="CDO enabled by default for capella < 6.0.0",
             ),
         ),
@@ -183,6 +187,8 @@ def test_model_backup_happy(
     ],
     indirect=True,
 )
-def test_model_backup_unhappy(t4c_backup_container):
+def test_model_backup_unhappy(t4c_backup_container_parametrized):
     with pytest.raises(RuntimeError):
-        conftest.wait_for_container(t4c_backup_container, "Backup finished")
+        conftest.wait_for_container(
+            t4c_backup_container_parametrized, "Backup finished"
+        )
