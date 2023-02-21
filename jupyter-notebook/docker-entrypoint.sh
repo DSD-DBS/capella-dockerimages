8799b45cfa3eb4163192aeba9f49c650e8feb73e
Arjan Molenaar <gaphor@gmail.com>
2023-02-21 12:41:11 +0100

    feat: set up jupyter env based on requirements file

--- jupyter-notebook/docker-entrypoint.sh
+++ jupyter-notebook/docker-entrypoint.sh
@@ -3,7 +3,14 @@
 # SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
 # SPDX-License-Identifier: Apache-2.0
 
+echo "---START_PREPARE_WORKSPACE---"
+
 mkdir -p "$NOTEBOOKS_DIR"
+
+test -f "$NOTEBOOKS_DIR/requirements.txt" || cp /etc/skel/requirements_template.txt "$NOTEBOOKS_DIR/requirements.txt"
+pip install -r "$NOTEBOOKS_DIR/requirements.txt" 2>&1 | tee "$NOTEBOOKS_DIR/installlog.txt"
+
+echo "---START_SESSION---"
 
 jupyter-lab --ip=0.0.0.0 \
     --port=$JUPYTER_PORT \
