b6af9f5485131f9718fd713532cf0857534e6716
MoritzWeber <kontakt@moritz-weber.net>
2023-04-04 17:34:44 +0200

    ci: Add license headers for more files

--- t4c/patch_t4c.sh
+++ t4c/patch_t4c.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-dockerimages contributors
+# SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
 # SPDX-License-Identifier: Apache-2.0
 
 if [[ -n "/opt/updateSite/patch_info.csv" ]];
