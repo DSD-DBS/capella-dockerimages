3161d3634ef2405dc52c3c2e92e6790d0ebe07ca
Dominik Lammers <info@dominik-lammers.de>
2023-06-12 15:23:20 +0200

    feat: Extend capella base by feature patches
    This commit enables the addition of feature patches not only to
    our T4C image but also to the Capella base image. We have
    transformed the `patch_t4c_sh` it into a more general `patch.sh`
    script, which is now integrated into the Capella base image.
    To apply feature patches to images built on the Capella base, it's
    necessary to ensure the availability of a directory containing the
    feature patches zip files and `patch_info.csv`. This directory then
    has to be provided in PATCH_DIR and then we only have to run patch.sh

--- capella/patch.sh
+++ capella/patch.sh
@@ -2,9 +2,9 @@
 # SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
 # SPDX-License-Identifier: Apache-2.0
 
-if [[ -n "/opt/updateSite/patch_info.csv" ]];
+if [[ -n "${PATCH_DIR:?}/patch_info.csv" ]];
 then
-    while IFS="," read -r t4c_patch_zip install_iu tag
+    while IFS="," read -r patch_zip install_iu tag
     do
         INSTALL_IU_JOIN=$(echo $install_iu | sed "s/ /,/g");
         /opt/capella/capella \
@@ -13,9 +13,8 @@ then
         -profile DefaultProfile \
         -tag $tag \
         -noSplash \
-        -repository jar:file:///opt/updateSite/$t4c_patch_zip!/ \
-        -installIU $INSTALL_IU_JOIN && \
-        chown -R techuser /opt/capella/configuration;
-    done < /opt/updateSite/patch_info.csv
+        -repository jar:file://${PATCH_DIR:?}/$patch_zip!/ \
+        -installIU $INSTALL_IU_JOIN;
+    done < ${PATCH_DIR:?}/patch_info.csv
 fi;
 exit 0;
