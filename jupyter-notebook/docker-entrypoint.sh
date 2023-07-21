dbf6d9fa53caafb53021aecf1f49149e0e771e5f
MoritzWeber <kontakt@moritz-weber.net>
2023-07-21 09:09:06 +0200

    feat(jupyter): Use global venv and update requirements from template
    - Added a `run-jupyter-notebook` target to the Makefile
    - Install dependencies from requirements_template per default
    - Update depdendencies automatically during container startup
    - Use a global venv instead of `--break-system-packages`
    - Change ownership of pip packages from root to techuser
    
    Resolves #178

--- jupyter-notebook/docker-entrypoint.sh
+++ jupyter-notebook/docker-entrypoint.sh
@@ -1,14 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
 # SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
 
 echo "---START_PREPARE_WORKSPACE---"
 
 mkdir -p "$NOTEBOOKS_DIR"
 
 test -f "$NOTEBOOKS_DIR/requirements.txt" || cp /etc/skel/requirements_template.txt "$NOTEBOOKS_DIR/requirements.txt"
-pip install --break-system-packages -r "$NOTEBOOKS_DIR/requirements.txt" 2>&1 | tee "$NOTEBOOKS_DIR/installlog.txt"
+pip install -U -r "$NOTEBOOKS_DIR/requirements.txt" -r /etc/skel/requirements_template.txt 2>&1 | tee "$NOTEBOOKS_DIR/installlog.txt"
 
 echo "---START_SESSION---"
 
