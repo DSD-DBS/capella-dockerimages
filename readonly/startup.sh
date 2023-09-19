843d6ace883f367fcf21f1b6ea3cac6785ac4e33
MoritzWeber <kontakt@moritz-weber.net>
2023-09-19 10:49:48 +0200

    fix: Fail if RMT_PASSWORD is unset
    When RMT_PASSWORD wasn't set, openssl was waiting for the argument,
    This could cause the Docker container to hang up.

--- readonly/startup.sh
+++ readonly/startup.sh
@@ -3,9 +3,10 @@
 # SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
 # SPDX-License-Identifier: Apache-2.0
 
-set -ex
+set -exuo pipefail
+
 line=$(grep techuser /etc/shadow);
-echo ${line%%:*}:$(openssl passwd -6 -salt $(openssl rand -base64 16) $RMT_PASSWORD):${line#*:*:} > /etc/shadow;
+echo ${line%%:*}:$(openssl passwd -6 -salt $(openssl rand -base64 16) "${RMT_PASSWORD:?}"):${line#*:*:} > /etc/shadow;
 unset RMT_PASSWORD
 
 # Prepare Workspace
--- remote/startup.sh
+++ remote/startup.sh
@@ -3,11 +3,12 @@
 # SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
 # SPDX-License-Identifier: Apache-2.0
 
-set -e
+set -exuo pipefail
+
 if [ "$(whoami)" == "root" ] || [ "$(whoami)" == "techuser" ];
 then
     line=$(grep techuser /etc/shadow);
-    echo ${line%%:*}:$(openssl passwd -6 -salt $(openssl rand -base64 16) $RMT_PASSWORD):${line#*:*:} > /etc/shadow;
+    echo ${line%%:*}:$(openssl passwd -6 -salt $(openssl rand -base64 16) "${RMT_PASSWORD:?}"):${line#*:*:} > /etc/shadow;
 else
     echo "Only techuser and root are supported as users.";
     exit 1;
